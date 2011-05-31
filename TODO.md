ef94bb2782268aca6bfe4a77b63e6f5f6f47c088
Korny Sietsma <korny@sietsma.com>
2011-05-31 11:25:59 +1000

    no longer loads request token at startup, as this caused errors with multiple users, now loads as needed, stores request token in session temporarily. Also a pile of cleanups based on feedback from @weavejester and others - some still in progress

--- TODO.md
+++ TODO.md
@@ -1,7 +1,10 @@
 # TODO
 
-* remove hardcoded port - should be able to get the port from the request (though this will require lazy fetching of request tokens)
+* remove hardcoded port - should be able to get the port from the request
 * consider making the initial auth handshake happen lazily, rather than at startup time
 * add a proper jetty runner, and maybe a way to build a war file?
 * split main file up - though it's nice for examples to be a single file, it's a bit unstructured
 * error handling!  Especially, handling simple things like when the user clicks "no" on twitter!
+* get rid of stateful sessions - we only write to the session from one spot, so could use default sessions
+* consider reverting to non-snapshot version of clj-oauth (first attempt failed, should try again!)
+* try to get rid of ruby/node dependencies
--- src/twitter_example/core.clj
+++ src/twitter_example/core.clj
@@ -5,19 +5,16 @@
     ring.middleware.stacktrace
     ring.middleware.session
     sandbar.stateful-session
-    [ring.util.response :only [redirect]]
-    )
+    [ring.util.response :only [redirect response content-type status]])
   (:require [compojure.route :as route]
     [compojure.handler :as handler]
     [clj-json.core :as json]
     [oauth.client :as oauth]
     [clojure.java.io :as io]
-    twitter
-    ))
+    twitter))
 
 (defn file-as-json [path]
-  (first (json/parsed-seq (io/reader (io/resource path)) true))
-  )
+  (first (json/parsed-seq (io/reader (io/resource path)) true)))
 
 (def config-data (file-as-json "config/config.json"))
 
@@ -35,72 +32,84 @@
 
 (def oauth-response-callback (str "http://" (:host config-data) ":" (:port config-data) oauth-response-path))
 
-; request token must be fetched from Twitter - currently at startup, good candidate for doing lazily
-(def request-token
+(defn twitter-request-token
+  "fetch request token from twitter for a new user oauth dance"
+  []
   (do
     (println "requesting request token from Twitter")
     (oauth/request-token consumer oauth-response-callback)))
 
-; callback url is built using the retrieved request token
-(def callback-uri
+(defn callback-uri
+  "generate callback uri for a given request"
+  [request-token]
   (oauth/user-approval-uri consumer
     (:oauth_token request-token)))
 
-; access token for a user can be found once they've danced the OAuth dance, and we have a verifier
-(defn access-token-response [verifier] (oauth/access-token consumer
-  request-token
-  verifier))
+(defn access-token-response
+  "get access token from twitter once twitter has redirected back to the app"
+  [request-token verifier]
+  (oauth/access-token consumer
+    request-token
+    verifier))
 
-(defn json-response [data & [status]]
-  {:status (or status 200)
-   :headers {"Content-Type" "application/json"}
-   :body (json/generate-string data)})
+(defn json-response
+  "return a JSON formatted Ring response"
+  [data]
+  (-> (response (json/generate-string data))
+    (content-type "application/json")))
 
-(defn filter-tweet [tweet]
+(defn filtered-tweet
   "filter out just the parts of the tweet wanted by the UI"
+  [tweet]
   {:screen-name (get-in tweet [:user :screen_name]) :tweet (:text tweet)})
 
 (defroutes main-routes
   (GET "/" [] (resource "public/index.html"))
   (GET oauth-response-path [oauth_token oauth_verifier]
-    (let [resp (access-token-response oauth_verifier)]
+    (let [request-token (session-get :request-token)
+          resp (access-token-response request-token oauth_verifier)]
       (session-put! :twitter-oauth resp)
-      (redirect "/")
-      )
-    )
-  (GET "/auth/tweets.json" [:as resp]
-    (let [oauth (:twitter-oauth resp)]
-      (twitter/with-oauth consumer (:oauth_token oauth) (:oauth_token_secret oauth)
-        (json-response {"auth" true "name" (:screen_name oauth) "tweets" (map filter-tweet (twitter/home-timeline))})
-        )))
-  (GET "/auth/status.json" [:as resp]
+      (redirect "/")))
+  (GET "/auth/tweets.json" {oauth :twitter-oauth}
+    (twitter/with-oauth consumer (:oauth_token oauth) (:oauth_token_secret oauth)
+      (json-response {"auth" true "name" (:screen_name oauth) "tweets" (map filtered-tweet (twitter/home-timeline))})))
+  (GET "/auth/status.json" {oauth :twitter-oauth}
     (json-response
-      (let [oauth (:twitter-oauth resp)]
-        {"name" (:screen_name oauth)}
-        )
-      ))
+      {"name" (:screen_name oauth)}))
   (route/resources "/")
   (route/not-found "Page not found"))
 
-(defn with-oauth-check [handler]
+(defn wrap-oauth
   "middelware wrapper for twitter oauth credential check
    only checks requests starting with '/auth'
    if signed in, stores credentials in :twitter-oauth and continues
    if not signed in, returns a 401 error, including the callback URL needed to sign in,
    so clients can prompt the user to start the OAuth dance!"
-  (fn [req]
-    (if (re-matches #"/auth/.*" (:uri req))
+  [handler]
+; suggested routing - not working yet!
+;  (routes
+;    (ANY "/auth/*" request
+;      (if-let [oauth (session-get :twitter-oauth)]
+;        (handler (assoc request :twitter-oauth oauth))
+;        (let [request-token (twitter-request-token)
+;              auth-url (callback-uri request-token)]
+;          (session-put! :request-token request-token)
+;          (-> (json-response {:message "twitter not authorized" :authUrl auth-url})
+;            (status 401)))))
+;    handler))
+  (fn [request]
+    (if (re-matches #"/auth/.*" (:uri request))
       (if-let [oauth (session-get :twitter-oauth)]
-        (handler (assoc req :twitter-oauth oauth))
-        (json-response {:message "twitter not authorized" :authUrl callback-uri} 401)
-        )
-      (handler req)
-      ))
-  )
+        (handler (assoc request :twitter-oauth oauth))
+        (let [request-token (twitter-request-token)
+              auth-url (callback-uri request-token)]
+          (session-put! :request-token request-token)
+          (-> (json-response {:message "twitter not authorized" :authUrl auth-url})
+            (status 401))))
+      (handler request))))
 
 (def app
   (-> (handler/site main-routes)
-    (with-oauth-check)
+    (wrap-oauth)
     (wrap-stateful-session)
-    (wrap-json-params)
-    (wrap-stacktrace)))
+    (wrap-json-params)))
