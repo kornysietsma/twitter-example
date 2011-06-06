966282dc3a2efcb42a4f3bd6b3b4a8b4dad8b1d3
Korny Sietsma <korny@sietsma.com>
2011-06-06 11:17:42 +1000

    first test with prerequisite\!

--- test/twitter_example/core_test.clj
+++ test/twitter_example/core_test.clj
@@ -1,6 +1,9 @@
 (ns twitter-example.core-test
-  (:use midje.sweet)
-  (:use twitter-example.core)
+  (:use
+    midje.sweet
+    twitter-example.core
+    compojure.core
+    [compojure.response :only [resource]])
   (:require
     [clj-json.core :as json]
     [clojure.java.io :as io]))
@@ -14,3 +17,12 @@
   (filtered-tweet {}) => {:screen-name nil, :tweet nil}
   (filtered-tweet {:text "tweet", :user {:screen_name "tweety"}}) => {:screen-name "tweety", :tweet "tweet"}
   (filtered-tweet single-tweet-fixture) => {:screen-name "izaloko", :tweet "qu por qu kieres saver como poner pablito"})
+
+(facts "about the main routes"
+  "Bare request returns index file"
+  (main-routes {:uri "/" :request-method :get})
+  => (contains {:status 200
+                :body #"stuff"})
+  (provided
+    (resource "public/index.html") => "stuff")
+  )
